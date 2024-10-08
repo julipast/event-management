21f41e02c9c12b55a819e7ec9ca156e9703ed3da
julipast <julipast1107@gmail.com>
2024-10-08 23:11:18 +0300

    relations2

--- app/Http/Controllers/Api/AttendeeController.php
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -4,21 +4,28 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\AttendeeResource;
+use App\Http\Traits\CanLoadRelationships;
 use App\Models\Attendee;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
 class AttendeeController extends Controller
 {
+
+    use CanLoadRelationships;
+
+    private array $relations = ['user'];
+
     /**
      * Display a listing of the resource.
      */
     public function index(Event $event)
     {
-        $attendees = $event->attendees()->latest();
-        return AttendeeResource::collection(
-            $attendees->paginate()
+        $attendees = $this->loadRelationships(
+            $event->attendees()->latest()
         );
+
+        return AttendeeResource::collection($attendees->paginate());
     }
 
     /**
@@ -26,9 +33,11 @@ class AttendeeController extends Controller
      */
     public function store(Request $request, Event $event)
     {
-        $attendee = $event->attendees()->create([
-            'user_id' => 1
-        ]);
+        $attendee = $this->loadRelationships(
+            $event->attendees()->create([
+                'user_id' => 1
+            ])
+        );
 
         return new AttendeeResource($attendee);
     }
@@ -38,7 +47,7 @@ class AttendeeController extends Controller
      */
     public function show(Event $event, Attendee $attendee)
     {
-        return new AttendeeResource($attendee);
+        return new AttendeeResource($this->loadRelationships($attendee));
     }
 
     /**
@@ -54,7 +63,7 @@ class AttendeeController extends Controller
      */
     public function destroy(string $event, Attendee $attendee)
     {
-       $attendee->delete();
-       return response(null, 204);
+        $attendee->delete();
+        return response(null, 204);
     }
 }
