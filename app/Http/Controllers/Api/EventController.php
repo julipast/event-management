b4c27e1c9e46ae89377ec1d20c7dbfbb544f3b6d
julipast <julipast1107@gmail.com>
2024-10-09 23:30:33 +0300

    fix-route

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -13,6 +13,7 @@ class EventController extends Controller
     use CanLoadRelationships;
     private array $relations = ['user', 'attendees', 'attendees.user'];
 
+
     /**
      * Display a listing of the resource.
      */
@@ -39,7 +40,7 @@ class EventController extends Controller
                 'start_time' => 'required|date',
                 'end_time' => 'required|date|after:start_time',
             ]),
-            'user_id' => 1
+            'user_id' => $request->user()->id
         ]);
 
         return new EventResource($this->loadRelationships($event));
--- routes/api.php
+++ routes/api.php
@@ -7,12 +7,35 @@ use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/login', [AuthController::class, 'login']);
 
-Route::apiResource('events', EventController::class);
-Route::apiResource('events.attendees', AttendeeController::class)
-    ->scoped()->except(['update']);
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout']);
+
+
+Route::get('events', [EventController::class, 'index']);
+Route::get('events/{event}', [EventController::class, 'show']);
+
+Route::get('events/{event}/attendees', [AttendeeController::class, 'index']);
+Route::get('events/{event}/attendees/{attendee}', [AttendeeController::class, 'show']);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    // Events
+    Route::post('events', [EventController::class, 'store']);
+    Route::put('events/{event}', [EventController::class, 'update']);
+    Route::delete('events/{event}', [EventController::class, 'destroy']);
+
+    // Event Attendees
+    Route::delete('events/{event}/attendees/{attendee}', [AttendeeController::class, 'destroy']);
+    Route::post('events/{event}/attendees', [AttendeeController::class, 'store']);
+});
+
+
+Route::fallback(function () {
+    return response()->json(['message' => 'This endpoint was not found!'], 404);
+});
